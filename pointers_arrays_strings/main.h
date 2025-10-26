cd64ff7ca3569999f2f395d7a65d8b45a108436f
Hanin157 <alshaneen856@gmail.com>
2025-10-26 20:16:51 +0000

    Pointers/Arrays/Strings: add 3-puts implementation

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,10 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
-
-void reset_to_98(int *n);
-
-#endif /* MAIN_H */
-void swap_int(int *a, int *b);
-int _strlen(char *s);
+int _putchar(char c);
 void _puts(char *str);
-void print_rev(char *s);
+#endif /* MAIN_H */
