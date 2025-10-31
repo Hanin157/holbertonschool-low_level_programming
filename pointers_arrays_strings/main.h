c8aae9ed1b8c663c3c99b93e369bde5ee82dff55
Hanin157 <alshaneen856@gmail.com>
2025-10-31 23:06:42 +0000

    Add 0-memset task

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -1,11 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
-int _putchar(char c);
-void _puts(char *str);
-#endif /* MAIN_H */
-void print_rev(char *s);
-void rev_string(char *s);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif
+
