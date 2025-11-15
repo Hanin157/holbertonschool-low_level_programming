a64440826ce825a58dd679b37d03a68b2d149086
Hanin157 <alshaneen856@gmail.com>
2025-11-15 18:45:47 +0000

    0x0E-function_pointers: task 1 - implement array_iterator

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
+#include <stddef.h>
+
 /**
  * print_name - prints a name using a callback
  * @name: pointer to char name
@@ -9,5 +11,15 @@
  * Description: Calls the function @f with @name if both are non-NULL.
  */
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - executes a function on each element of an array
+ * @array: pointer to the first element of the integer array
+ * @size: number of elements in the array
+ * @action: pointer to function to apply to each element
+ *
+ * Description: Calls @action on each element of @array if both are non-NULL.
+ */
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
