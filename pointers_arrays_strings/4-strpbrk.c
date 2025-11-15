dfbd936e1c9b1e123a99baa41f4eca49a909ac10
Hanin157 <alshaneen856@gmail.com>
2025-11-15 22:35:50 +0000

    0x07-pointers_arrays_strings: task 4 - implement _strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search in
+ * @accept: the set of bytes to search for
+ * Return: pointer to the first matching byte in s, or NULL if none
+ */
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i;
+unsigned int j;
+
+i = 0;
+while (s[i] != '\0')
+{
+j = 0;
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+return (s + i);
+j++;
+}
+i++;
+}
+return (0);
+}
