63af30e076ca40f30f987fb7d3a675b4019e8307
Hanin157 <alshaneen856@gmail.com>
2025-11-15 18:22:20 +0000

    Fix 0-print_name to match checker expectations

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,17 +1,17 @@
+#include <stdlib.h>
 #include "function_pointers.h"
 
 /**
- * print_name - prints a name using a provided callback
+ * print_name - prints a name using a function pointer
  * @name: pointer to the name string
- * @f: pointer to function (char *) -> void
+ * @f: pointer to function that takes a char * and returns void
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-if (name == (void *)0 || f == (void *)0)
-{
+if (name == NULL || f == NULL)
 return;
-}
+
 f(name);
 }
