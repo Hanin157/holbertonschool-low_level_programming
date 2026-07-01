9b9e06d9309d8966b5b0d164755c02ab68f7bc75
Hanin157 <alshaneen856@gmail.com>
2026-07-01 09:29:21 +0000

    Add print_diagsums function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,4 +15,5 @@ char *_strncpy(char *dest, char *src, int n);
 void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
+void print_diagsums(int *a, int size);
 #endif /* MAIN_H */
